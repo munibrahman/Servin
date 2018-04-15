a9b70745491c1048c564bce67fc239b4993188d9
Voltic Labs <developer@volticlabs.com>
2018-04-14 22:32:06 -0600

    Initial Commit

---
+++ ServinV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServinV2
+//
+//  Created by Developer on 2018-04-14.
+//  Copyright © 2018 Voltic Labs Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
